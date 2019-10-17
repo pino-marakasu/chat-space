d5a566ee11a8cedadada82ae022364a18080182a
pino-marakasu <ayumu.t.1607@gmail.com>
2019-10-17 20:44:56 +0900

    Messade create1

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   root 'groups#index'
-  devise_for :users, contollers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
-  }
+  devise_for :users
+  # , contollers: {
+  #   registrations: 'users/registrations',
+  #   sessions: 'users/sessions'
+  # }
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
-
-  devise_scope :user do
-    get "sign_in", to: "users/sessions#new"
-    get "sign_out", to: "users/sessions#destroy"
-  end
- 
-  
 end
+  # devise_scope :user do
+  #   get "sign_in", to: "users/sessions#new"
+  #   get "sign_out", to: "users/sessions#destroy"
+  # end
+ 
