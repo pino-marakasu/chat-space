3f759534a64fe89450c03651f38e99b5b4199a77
pino-marakasu <ayumu.t.1607@gmail.com>
2019-10-31 18:00:05 +0900

    自動更新　TASK3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -32,15 +32,12 @@ $(function(){
       processData: false,
       contentType: false
     })
-    .done(function(data){
-      var html = buildHTML(data);
+    .done(function(message){
+      var html = buildHTML(message);
       $('.messages').append(html);
       $('form')[0].reset();
       $('form').prop('disabled',false);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      // function ScrollToNewMessage(){
-      //   $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      // }
     })
     .fail(function(){
       alert('error');
@@ -51,24 +48,25 @@ $(function(){
 
   var reloadMessages = function() {
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      var last_message_id = $('.message:last').data("id");
+      var last_message_id = $('.message:last').data("message-id");
       $.ajax({
         url: 'api/messages',
         type: 'get',
         dataType: 'json',
         data: {id: last_message_id}
       })
-      .done(function(messages) {
+      .done(function (messages) {
         var insertHTML = '';
-        messages.forEach(function(message){
-          insertHTML = buildHTML(message);
+        messages.forEach(function (message) {
+          var insertHTML = buildHTML(message);
           $('.messages').append(insertHTML);
-        }); 
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       })
       .fail(function() {
         alert('自動更新に失敗しました');
       });
     };
   }
-
+  setInterval(reloadMessages, 8000);
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,14 @@
 Rails.application.routes.draw do
-  root 'groups#index'
-  root 'users#index'
-  
   devise_for :users
-  # , contollers: {
-  #   registrations: 'users/registrations',
-  #   sessions: 'users/sessions'
-  # }
+  root 'groups#index'
+ 
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
-    
+
     namespace :api do
       resources :messages, only: :index, defaults: { format: 'json' }
     end
   end
 end
-  # devise_scope :user do
-  #   get "sign_in", to: "users/sessions#new"
-  #   get "sign_out", to: "users/sessions#destroy"
-  # end
- 
+  
